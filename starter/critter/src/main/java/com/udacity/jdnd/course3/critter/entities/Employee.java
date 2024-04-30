418caec6700836f29c9090285c353db99267a6f7
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-30 15:50:54 +0530

    feat: Task 3 & 4: Entities Design & Creation

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entities/Employee.java
@@ -0,0 +1,60 @@
+package com.udacity.jdnd.course3.critter.entities;
+
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.Nationalized;
+
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+
+    @ElementCollection
+    private Set<EmployeeSkill> skills;
+    @ElementCollection
+    private Set<DayOfWeek> daysAvailable;
+
+    public Employee() {};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entities/Pet.java
@@ -0,0 +1,88 @@
+package com.udacity.jdnd.course3.critter.entities;
+
+import com.udacity.jdnd.course3.critter.pet.PetType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Nationalized;
+
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PetType type;
+
+    @Nationalized
+    private String name;
+
+    private long ownerId;
+    private LocalDate birthDate;
+
+    @Column(length = 500)
+    private String notes;
+
+    @ManyToOne(fetch = FetchType.LAZY) //many pets can belong to one owner
+    @JoinColumn(name = "customer_id", referencedColumnName = "id") // map the join column in the customer table
+    private Customer customer;
+
+    public Pet() {};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
