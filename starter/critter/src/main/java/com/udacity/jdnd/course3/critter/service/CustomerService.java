509c433d387747a41e2d2db58301779d26842a9c
Krupesh Anadkat <kmanadkat@gmail.com>
2024-05-02 16:17:15 +0530

    feat: Marked All Endpoints Transactional

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
@@ -4,11 +4,13 @@ import com.udacity.jdnd.course3.critter.entities.Customer;
 import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
